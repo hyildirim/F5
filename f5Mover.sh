733cfc5316c586e937571d32e4d0fe6fbf13bcee
hyildirim <hyildirim@users.noreply.github.com>
2016-07-05 16:13:50 -0500

    Added backupdir creation

--- f5Mover.sh
+++ f5Mover.sh
@@ -9,6 +9,23 @@ DATE=`date +%Y-%m-%d`
 
 echo "INFO: Saving current configuration to /var/local/ucs/beforeMaintenance-$DATE.ucs"
 
+BACKUPDIR="/root/configBackups/$DATE";
+
+
+if [ ! -d $BACKUPDIR ]; then
+        mkdir -p $BACKUPDIR
+        echo "Creating backup directory $BACKUPDIR"
+fi
+
+echo "INFO: Saving important configuration files [/config/*.conf] to $BACKUPDIR"
+cp /config/*.conf $BACKUPDIR
+if [ -d "/config/partitions" ]; then
+        echo "Copying partition configs to $BACKUPDIR"
+        cp -R /config/partitions $BACKUPDIR
+fi
+
+
+
 #tmsh save sys ucs beforeMaintenance-$DATE.ucs
 
 # This is where you can do your excludes or simply edit customerConfiguration.conf file and
@@ -25,23 +42,12 @@ tmsh list ltm pool one-line >> /shared/tmp/customerConfiguration.conf
 echo "INFO: Export all node objects under /Common"
 tmsh list ltm node one-line >> /shared/tmp/customerConfiguration.conf
 
-echo "INFO: Downloading f5PartitionMover script from github"
+echo "INFO: Downloading the script from github"
 curl -o /root/f5PartitionMover.pl -s https://raw.githubusercontent.com/hyildirim/F5/master/f5PartitionMover.pl
-
-echo "INFO: Downloading f5MoveObjects script from github"
-curl -o /root/f5MoveObjects.pl -s https://raw.githubusercontent.com/hyildirim/F5/master/f5MoveObjects.pl
-
 echo "INFO: Script was saved under /root/f5PartitionMover.pl"
-echo "INFO: Scrtip was saved under root/f5MoveObjects.pl"
-
-
-echo "---------------------------------------------------------------------------------------------"
 echo "INFO: Examine /shared/tmp/customerConfiguration.conf VERY CAREFULLY and EDIT if necessary"
 echo "INFO: if you would like to exclude certain objects from being moved to another partition"
-echo "---------------------------------------------------------------------------------------------"
 echo "INFO: when ready, execute perl /root/f5PartitionMover.pl /shared/tmp/customerConfiguration.conf"
 echo "INFO: This will generate the changes under /root/newCustomerConfig.txt"
 echo "INFO: Examine the configuration changes"
 echo "INFO: When ready execute f5MoveObjects.pl /root/newCustomerConfig.txt"
-echo "---------------------------------------------------------------------------------------------"
-
